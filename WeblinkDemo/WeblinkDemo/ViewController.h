eeb5679ba4b075a4a2c3bb2717eb2f62eb587212
Abhishek Shukla <abhishekshukla@InnovationM-48.local>
2015-12-13 21:57:47 +0530

    WebLink Demo
    WebService Hit

---
+++ WeblinkDemo/WeblinkDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeblinkDemo
+//
+//  Created by Abhishek Shukla on 13/12/15.
+//  Copyright © 2015 InnovationM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
